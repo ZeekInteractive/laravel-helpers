916a6230ee3a97a1d50419c182816d3ea6edae16
Aaron Holbrook <AaronHolbrook@users.noreply.github.com>
2020-10-30 15:43:32 -0500

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -4,11 +4,17 @@ use Illuminate\Support\Str;
 
 /**
  * Retrieve sql with bindings from a query.
- * 
+ *
  * @param \Illuminate\Database\Query\Builder|\Illuminate\Database\Eloquent\Builder $query
+ *
  * @return string
  */
 function to_sql($query)
 {
-    return Str::replaceArray('?', $query->getBindings(), $query->toSql());
+	$bindings = $query->getBindings();
+	$rawSql   = $query->toSql();
+
+	$rawSql = str_replace('?', "'?'", $rawSql);
+
+	return Str::replaceArray('?', $bindings, $rawSql);
 }
